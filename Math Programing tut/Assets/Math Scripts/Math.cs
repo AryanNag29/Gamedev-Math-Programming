43a3557da052a0d5d3791f2f3a0cab612f524f29
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-09-09 23:14:44 +0530

    added two new game object a and b

--- Math Programing tut/Assets/Math Scripts/Math.cs
+++ Math Programing tut/Assets/Math Scripts/Math.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 
 public class Math : MonoBehaviour
 {
+    public Transform a;
+    public Transform b;
+
     //Draw in scene view
     void OnDrawGizmos()
     {
-        Gizmos.DrawSphere(transform.position, 5);
+        Vector2 A = a.position;
+        Vector2 B = b.position;
+
+        Gizmos.color = Color.yellow;
+        //Gizmos.DrawSphere(transform.position, 1);
     }
 }
