6c500591267c0f4e3ef420a1e179ddaa72c66c7f
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-09-20 00:57:12 +0530

    Handles has more functions then gizmos

--- Math Programing tut/Assets/Math Scripts/Math_Logic.cs
+++ Math Programing tut/Assets/Math Scripts/Math_Logic.cs
@@ -1,6 +1,15 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Math_Logic : MonoBehaviour
 {
     public Transform P1;
+
+    public float radius = 1f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(default, radius);
+        //Handles.DrawWireArc(); Handles has more functions then gizmos 
+    }
 }
