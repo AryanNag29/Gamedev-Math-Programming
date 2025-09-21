b3d421bc61e9efbc66bc7c88121d0546c1c8d0dc
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-09-22 00:27:48 +0530

    is trigger working

--- Math Programing tut/Assets/Math Scripts/Math_Logic.cs
+++ Math Programing tut/Assets/Math Scripts/Math_Logic.cs
@@ -1,3 +1,4 @@
+using Unity.InferenceEngine;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,10 +8,38 @@ public class Math_Logic : MonoBehaviour
 
     public float radius = 1f;
 
+    public float dist;
+
+    public bool isTrigger = false;
+
     void OnDrawGizmos()
     {
         Vector3 center = transform.position;
-        Gizmos.DrawWireSphere(center, radius);
         //Handles.DrawWireArc(); Handles has more functions then gizmos 
+
+        if (P1 == null)
+        {
+            return;
+        }
+
+        Vector3 playerpos = P1.position;
+
+        dist = Vector3.Distance(center, playerpos);
+        Vector3 playerDist = P1.transform.position;
+        Gizmos.DrawLine(center, playerDist);
+        //istrigger Condition
+        if (dist <= radius)
+        {
+            isTrigger = true;
+        }
+        else
+        {
+            isTrigger = false;
+        }
+        
+        Gizmos.color = isTrigger ? Color.red : Color.white;
+        Gizmos.DrawWireSphere(center, radius);
+
+
     }
 }
