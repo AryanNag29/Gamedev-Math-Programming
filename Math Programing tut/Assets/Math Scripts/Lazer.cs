3e4b6404fbf37b2e50059cdafa96a4546c51a260
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-09-27 01:05:32 +0530

    implemented multiple bounces in lazer

--- Math Programing tut/Assets/Math Scripts/Lazer.cs
+++ Math Programing tut/Assets/Math Scripts/Lazer.cs
@@ -11,20 +11,25 @@ public class Lazer : MonoBehaviour
         Vector2 origin = transform.position;
         Vector2 dir = transform.right;
         Ray ray = new Ray(origin, dir);
+        int maxBounces = 40;
 
-
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            Gizmos.DrawSphere(hit.point, 0.1f);
-            //draw line on x axis
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(origin, hit.point);
-
-            Vector2 Reflected = Reflect(ray.direction, hit.normal);
-            //draw line 
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(hit.point, (Vector2)hit.point + Reflected);
-        }
+    for (int i = 0; i < maxBounces; i++)
+    {
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Gizmos.DrawSphere(hit.point, 0.05f);
+                //draw line on x axis
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(ray.origin, hit.point);
+                Vector2 Reflected = Reflect(ray.direction, hit.normal);
+                //draw line 
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(hit.point, (Vector2)hit.point + Reflected);
+                ray.direction = Reflected;
+                ray.origin = hit.point;
+            }
+            else break;        
+    }
     }
 
     Vector2 Reflect(Vector2 inDir, Vector2 normal)
