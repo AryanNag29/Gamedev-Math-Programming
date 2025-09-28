b880f77e6ad72898f4b3e3dfa908c23c83b67a90
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-09-29 00:15:46 +0530

    Made function to transform from local to world space

--- Math Programing tut/Assets/Math Scripts/Transform Vectors.cs
+++ Math Programing tut/Assets/Math Scripts/Transform Vectors.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class TransformVectors : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+
+    #region Variables
+
+    //variables
+    public Vector2 localCoord;
+
+    #endregion
+
+    #region Functions
+
+    Vector2 LocalToWorld(Vector2 local) {
+        Vector2 position = transform.position;
+        position += local.x * (Vector2)transform.right;
+        position += local.x * (Vector2)transform.up;
+        return position;
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region Gizmos
+
+    void OnDrawGizmos()
     {
-        
+        Vector2 worldpos = LocalToWorld(localCoord);
+        Gizmos.DrawSphere(worldpos, 0.1f);
     }
+    #endregion
 }
