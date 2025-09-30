1064bf770227fe1727a5862cd36c46487b5b3d3a
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-09-30 07:02:16 +0530

    line from world origin to local space

--- Math Programing tut/Assets/Math Scripts/Transform Vectors.cs
+++ Math Programing tut/Assets/Math Scripts/Transform Vectors.cs
@@ -7,14 +7,18 @@ public class TransformVectors : MonoBehaviour
 
     //variables
     public Vector2 localCoord;
+    public Vector2 worldpos;
 
     #endregion
 
     #region Functions
 
     Vector2 LocalToWorld(Vector2 local) {
+        //made the variable
         Vector2 position = transform.position;
+        //store position + localcoord.x * transform.right
         position += local.x * (Vector2)transform.right;
+        //store position localcoord.y * transform.up
         position += local.x * (Vector2)transform.up;
         return position;
     }
@@ -25,8 +29,12 @@ public class TransformVectors : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        Vector2 worldpos = LocalToWorld(localCoord);
+        worldpos = LocalToWorld(localCoord);
+        Gizmos.color = Color.red;
         Gizmos.DrawSphere(worldpos, 0.1f);
+        //local to world space line
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(default, worldpos);
     }
     #endregion
 }
