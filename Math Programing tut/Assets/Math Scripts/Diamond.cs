ca1e8bf9e0e8162079d1db62bb5e99546dd0da66
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-10-03 23:50:03 +0530

    made diamond for the todays practical in unity 3d

---
+++ Math Programing tut/Assets/Math Scripts/Diamond.cs
@@ -0,0 +1,28 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Diamond : MonoBehaviour
+{
+    public Transform b;
+    public Transform c;
+    public Transform d;
+
+    void OnDrawGizmos()
+    {
+        transform.position = new Vector3(4, 1, 0);
+        b.transform.position = new Vector3(2, 4, 0);
+        c.transform.position = new Vector3(4, 7, 0);
+        d.transform.position = new Vector3(6, 4, 0);
+
+        Vector3 A = transform.position;
+        Vector3 B = b.position;
+        Vector3 C = c.position;
+        Vector3 D = d.position;
+
+        Gizmos.DrawLine(A, B);
+        Gizmos.DrawLine(B, C);
+        Gizmos.DrawLine(C, D);       
+        Gizmos.DrawLine(D, A);
+    }
+}
