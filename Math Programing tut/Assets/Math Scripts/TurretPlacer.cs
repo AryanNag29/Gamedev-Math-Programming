1f3491dfaf65a27f3110a79f3a209692b7d8e041
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-10-09 23:25:09 +0530

    made player pov turret

--- Math Programing tut/Assets/Math Scripts/TurretPlacer.cs
+++ Math Programing tut/Assets/Math Scripts/TurretPlacer.cs
@@ -1,13 +1,18 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class TurretPlacer : MonoBehaviour
 {
+    public Transform turret;
     void OnDrawGizmos()
     {
+        if (turret == null) return;
+
         Ray ray = new Ray(transform.position,transform.forward);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            
+            turret.position = hit.point;
+            turret.rotation = quaternion.LookRotation(ray.direction,transform.forward);
         }
     }
 }
