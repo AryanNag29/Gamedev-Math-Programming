5c6ab447a8a1370fa73b8b5a1044683c99b8a8ea
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-04 23:31:31 +0530

    aliened the turret using cross product

--- MathsNewTut/Assets/Script/Turretplacer.cs
+++ MathsNewTut/Assets/Script/Turretplacer.cs
@@ -13,10 +13,20 @@ public class Turretplacer : MonoBehaviour
         if(Physics.Raycast(ray,out RaycastHit hit))
         {
             turret.transform.position = hit.point;
-            turret.rotation = Quaternion.LookRotation(ray.direction);
+            
 
+            Vector3 yAxis = hit.normal;
+            //cross product for the turret
+            Vector3 xAxis = Vector3.Cross(yAxis, ray.direction).normalized;
+            Vector3 zAxis = Vector3.Cross(xAxis, yAxis);
+            turret.rotation = Quaternion.LookRotation(xAxis,yAxis);
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(hit.point, yAxis);
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(hit.point, xAxis);
             Gizmos.color = Color.blue;
-            Gizmos.DrawRay(hit.point, hit.normal);
+            Gizmos.DrawRay(hit.point, zAxis);
+            Gizmos.color = Color.white;
             Gizmos.DrawLine(ray.origin, hit.point);
         }
     }
