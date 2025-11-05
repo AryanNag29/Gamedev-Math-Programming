4049fa86032163f1db071e43080d6e71d790fe8e
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-05 22:46:45 +0530

    fixed the turret offset

--- MathsNewTut/Assets/Script/Turretplacer.cs
+++ MathsNewTut/Assets/Script/Turretplacer.cs
@@ -12,15 +12,15 @@ public class Turretplacer : MonoBehaviour
         
         if(Physics.Raycast(ray,out RaycastHit hit))
         {
-            turret.transform.position = hit.point;
+            turret.position = hit.point;
 
 
             Vector3 yAxis = hit.normal;
             //cross product for the turret
-            //Grahm-Schmidt orthonormalization
-            Vector3 xAxis = Vector3.Cross(yAxis, ray.direction).normalized;
-            Vector3 zAxis = Vector3.Cross(xAxis, yAxis);
-            turret.rotation = Quaternion.LookRotation(xAxis,yAxis);
+            //Gram-Schmidt orthonormalization
+            Vector3 xAxis = Vector3.Cross(yAxis, ray.direction).normalized; //bitangent 
+            Vector3 zAxis = Vector3.Cross(xAxis, yAxis); //tangents
+            turret.rotation = Quaternion.LookRotation(zAxis,yAxis);
             Gizmos.color = Color.green;
             Gizmos.DrawRay(hit.point, yAxis);
             Gizmos.color = Color.red;
