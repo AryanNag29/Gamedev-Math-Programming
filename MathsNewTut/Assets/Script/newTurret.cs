7281d0046654bff74e5d875cbfc4b5a612633302
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-08 22:28:23 +0530

    added gizmos rays for more understanding

--- MathsNewTut/Assets/Script/newTurret.cs
+++ MathsNewTut/Assets/Script/newTurret.cs
@@ -15,10 +15,17 @@ public class newTurret : MonoBehaviour
         {
             Turret.position = hit.point;
             Vector3 yAxis = hit.normal;
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(hit.point, yAxis);
             Vector3 xAxis = Vector3.Cross(yAxis, ray.direction).normalized;
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(hit.point, xAxis);
             Vector3 zAxis = Vector3.Cross(yAxis, xAxis);
-
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(hit.point, zAxis);
             Turret.rotation = quaternion.LookRotation(zAxis, yAxis);
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(ray.origin, hit.point);
         }
     }
 }
