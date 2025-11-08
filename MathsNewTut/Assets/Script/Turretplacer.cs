cde079cd669ea6e52d57b4548d1d02f0f3a7c3d1
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-08 22:32:27 +0530

    added region to code

--- MathsNewTut/Assets/Script/Turretplacer.cs
+++ MathsNewTut/Assets/Script/Turretplacer.cs
@@ -4,23 +4,26 @@ using UnityEngine.Timeline;
 
 public class Turretplacer : MonoBehaviour
 {
+    #region PublicReferences
     public Transform turret;
+    #endregion
+
+    #region Gizmos
     void OnDrawGizmos()
     {
         Ray ray = new Ray(transform.position, transform.forward);
         if (turret == null) return;
-        
-        if(Physics.Raycast(ray,out RaycastHit hit))
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
             turret.position = hit.point;
-
 
             Vector3 yAxis = hit.normal;
             //cross product for the turret
             //Gram-Schmidt orthonormalization
             Vector3 xAxis = Vector3.Cross(yAxis, ray.direction).normalized; //bitangent 
             Vector3 zAxis = Vector3.Cross(xAxis, yAxis); //tangents
-            turret.rotation = Quaternion.LookRotation(zAxis,yAxis);
+            turret.rotation = Quaternion.LookRotation(zAxis, yAxis);
             Gizmos.color = Color.green;
             Gizmos.DrawRay(hit.point, yAxis);
             Gizmos.color = Color.red;
@@ -31,4 +34,5 @@ public class Turretplacer : MonoBehaviour
             Gizmos.DrawLine(ray.origin, hit.point);
         }
     }
+    #endregion
 }
