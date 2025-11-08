9dbb83ac2c330bded3905d3249b04206773a5c4c
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-08 23:12:14 +0530

    using handles for our trigger

--- MathsNewTut/Assets/Script/CheesyScript.cs
+++ MathsNewTut/Assets/Script/CheesyScript.cs
@@ -1,6 +1,14 @@
+using UnityEditor;
 using UnityEngine;
 
 public class CheesyScript : MonoBehaviour
 {
-    
+    public float radius = 1;
+    public float height = 1;
+    [Range(0, 1)]
+    public float angThresh = 0.5f;
+    void OnDrawGizmos()
+    {
+        Handles.DrawWireDisc(transform.position, transform.up, radius);
+    }
 }
