602efa6e79cfa1da35a6196f5ee418750f9d1182
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-08 23:35:03 +0530

    Made a target game object for trigger

--- MathsNewTut/Assets/Script/CheesyScript.cs
+++ MathsNewTut/Assets/Script/CheesyScript.cs
@@ -3,12 +3,25 @@ using UnityEngine;
 
 public class CheesyScript : MonoBehaviour
 {
+    #region PublicReferences
+    public Transform target;
+    #endregion
+
+    #region Variables 
     public float radius = 1;
     public float height = 1;
-    [Range(0, 1)]
-    public float angThresh = 0.5f;
+    [Range(0, 1)] //quite usefull for the range base slider
+    public float angThresh = 0.5f; //not an actual
+    #endregion
+
+    #region Gizmos
     void OnDrawGizmos()
     {
-        Handles.DrawWireDisc(transform.position, transform.up, radius);
+        Vector3 origin = transform.position;
+        Vector3 up = transform.up;
+        Vector3 top = origin + up * height;
+        Handles.DrawWireDisc(origin, up, radius);
+        Handles.DrawWireDisc(top, up, radius);
     }
+    #endregion
 }
