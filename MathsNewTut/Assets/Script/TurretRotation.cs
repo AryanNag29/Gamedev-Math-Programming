c00259cd8d343a84a1ab64ea63c6d5697f81c810
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-14 01:48:12 +0530

    set the turret rotation according to the target when the trigger is true

--- MathsNewTut/Assets/Script/TurretRotation.cs
+++ MathsNewTut/Assets/Script/TurretRotation.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TurretRotation : MonoBehaviour
 {
     #region References
-
+    public Transform target;
+    public CheesyScript trigger;
+    public Transform gunTf;
     #endregion
 
     #region Variables
@@ -17,7 +20,15 @@ public class TurretRotation : MonoBehaviour
     #region Gizmos
     void OnDrawGizmos()
     {
-        
+        if (trigger.Contains(target.position))
+        {
+            //note: worldspace rotation
+            
+            Vector3 vecToTargetgun = target.position - gunTf.position;
+            gunTf.rotation = Quaternion.LookRotation(vecToTargetgun, transform.up);
+
+        }
+       
     }
     #endregion
 
