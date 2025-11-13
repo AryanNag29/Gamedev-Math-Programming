7aa3e891ef08c8584d243641734dbfb19acc07b6
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-14 01:23:11 +0530

    Made a Script for the turret rotation for the target

---
+++ MathsNewTut/Assets/Script/TurretRotation.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TurretRotation : MonoBehaviour
+{
+    #region References
+
+    #endregion
+
+    #region Variables
+
+    #endregion
+
+    #region Function
+
+    #endregion
+
+    #region Gizmos
+    void OnDrawGizmos()
+    {
+        
+    }
+    #endregion
+
+}
