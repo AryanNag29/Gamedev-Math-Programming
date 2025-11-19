5b0fab9af04d404f4df54ff5bfbf129d3f6b3bba
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-11-20 00:11:44 +0530

    use case of cos and sin function

--- MathsNewTut/Assets/Script/Trigo.cs
+++ MathsNewTut/Assets/Script/Trigo.cs
@@ -1,16 +1,22 @@
+using System;
+using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 
 public class Trigo : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    #region Variables
+    [Range(0,360)]
+    public float angDeg = 0f;
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    #region Gizmos
+    void OnDrawGizmos()
     {
-        
+        Handles.DrawWireDisc(Vector3.zero,Vector3.forward,1);
+        float angRad = angDeg * Mathf.Deg2Rad;
+        Vector2 v = new Vector2(Mathf.Cos(angRad),Mathf.Sin(angRad));
+        Gizmos.DrawRay(default,v);
     }
+    #endregion
 }
