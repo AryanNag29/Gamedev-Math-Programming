1838d17c76800c421f13a46d2c92fb898feaf5e6
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-12-03 23:20:00 +0530

    Drawing Clock on the editor

--- MathsNewTut/Assets/Script/ClockScript.cs
+++ MathsNewTut/Assets/Script/ClockScript.cs
@@ -1,11 +1,29 @@
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class ClockScript : MonoBehaviour
 {
+    #region variables
+    public float clockRadius = 1f;
+    public float angRed = 0f;
+    #endregion
+
+    #region Properties
+
+    Vector2 AngleToDis(float AngRed) => new Vector2(Mathf.Cos(AngRed), Mathf.Sin(AngRed));        
+
+    #endregion
+    
+    #region gizmos
     void OnDrawGizmos()
     {
+        Handles.DrawWireDisc(default, Vector3.forward,clockRadius);
+        Vector2 Second = AngleToDis(angRed);
+        
+        Gizmos.DrawRay(default,Second);
         
     }
+    #endregion
 
 }
