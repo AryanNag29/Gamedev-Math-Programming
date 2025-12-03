07fdf36ea80e26826ab2d87547c720f004a6426b
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-12-03 23:36:44 +0530

    Made the Second hand , hour hand and minute hand

--- MathsNewTut/Assets/Script/ClockScript.cs
+++ MathsNewTut/Assets/Script/ClockScript.cs
@@ -6,7 +6,10 @@ public class ClockScript : MonoBehaviour
 {
     #region variables
     public float clockRadius = 1f;
-    public float angRed = 0f;
+    public float angRed = 1f;
+    private float secHand = .85f;
+    private float minHand = .75f;
+    private float hourHand = .50f;
     #endregion
 
     #region Properties
@@ -19,9 +22,17 @@ public class ClockScript : MonoBehaviour
     void OnDrawGizmos()
     {
         Handles.DrawWireDisc(default, Vector3.forward,clockRadius);
-        Vector2 Second = AngleToDis(angRed);
+        Vector2 Second = AngleToDis(angRed*20f)*secHand;
+        Vector2 Minute = AngleToDis(angRed*45f)*minHand;
+        Vector2 Hour = AngleToDis(angRed*90f)*hourHand;
         
+        //DrawHands
+        Gizmos.color = Color.red;
         Gizmos.DrawRay(default,Second);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(default,Minute);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(default,Hour);
         
     }
     #endregion
