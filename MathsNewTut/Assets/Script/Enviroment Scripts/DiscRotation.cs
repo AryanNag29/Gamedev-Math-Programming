4758b01f931115c4cef0317125f8ecdee058faf2
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-03-31 02:11:04 +0530

    level design

--- MathsNewTut/Assets/Script/Enviroment Scripts/DiscRotation.cs
+++ MathsNewTut/Assets/Script/Enviroment Scripts/DiscRotation.cs
@@ -1,7 +1,24 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class DiscRotation : MonoBehaviour
 {
+    #region Variables
+
+    private float rotationSpeed = 10f;
+
+    #endregion
+
+    #region Functions
+
+    public void DiscRotationSpeed()
+    {
+        quaternion currentRotation = transform.rotation;
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0f);
+    }
+
+    #endregion
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +28,6 @@ public class DiscRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DiscRotationSpeed();
     }
 }
