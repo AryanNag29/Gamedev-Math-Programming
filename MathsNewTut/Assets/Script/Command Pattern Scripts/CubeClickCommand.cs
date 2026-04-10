ab886404d3c899baaff4b2f325a3a783d2c460a1
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-04-11 01:05:16 +0530

    make it mono behaviour

--- MathsNewTut/Assets/Script/Command Pattern Scripts/CubeClickCommand.cs
+++ MathsNewTut/Assets/Script/Command Pattern Scripts/CubeClickCommand.cs
@@ -1,12 +1,14 @@
+using System;
 using UnityEngine;
 
 //if you include interface type icommand now you have to implement the function included in 'Interface Icommand'
-public class CubeClickCommand : Icommand
+public class CubeClickCommand : MonoBehaviour,Icommand
 {
     //object of class gameObject and mesh renderer
     private GameObject _cube;
     private MeshRenderer _renderer;
-    
+
+    public CommandManager _CommandManager;
     //these are variable of type color
     private Color newColor;
     private Color prevColor;
@@ -20,6 +22,7 @@ public class CubeClickCommand : Icommand
 
         prevColor = _renderer.material.color;
         this.newColor = newColor; // same for this 
+        _CommandManager.AddCommand(this); 
     }
 
     public void Execute()
@@ -32,5 +35,10 @@ public class CubeClickCommand : Icommand
     {
         //assign prev color to the material whenever undo is clicked
         _renderer.material.color = prevColor;
+    }
+
+    public void Start()
+    {
+        _CommandManager = GetComponent<CommandManager>();
     }
 }
