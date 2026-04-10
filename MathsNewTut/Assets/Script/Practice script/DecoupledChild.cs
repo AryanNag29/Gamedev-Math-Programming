0f555c1ae469bc92456aa088d1cf1545775ec303
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-04-10 13:35:47 +0530

    Logic example for decoupled classes

--- MathsNewTut/Assets/Script/Practice script/DecoupledChild.cs
+++ MathsNewTut/Assets/Script/Practice script/DecoupledChild.cs
@@ -1,7 +1,24 @@
+using System;
 using UnityEngine;
 
 public class DecoupledChild : MonoBehaviour
 {
+    
+    void DisplayGameOver()
+    {
+        Debug.Log("Displaying Game Over UI via Event!");
+    }
+    
+    private void OnEnable()
+    {
+        DecoupledParent.OnPlayerDeath += DisplayGameOver;
+    }
+
+    private void OnDisable()
+    {
+        DecoupledParent.OnPlayerDeath -= DisplayGameOver;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- MathsNewTut/Assets/Script/Practice script/DecoupledParent.cs
+++ MathsNewTut/Assets/Script/Practice script/DecoupledParent.cs
@@ -1,7 +1,25 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DecoupledParent : MonoBehaviour
 {
+    public static event Action OnPlayerDeath ;
+
+    public void TakeDamage(float damage)
+    {
+        Die();
+    }
+    
+    void Die()
+    {
+        //logic
+        
+        //raise event 
+        //"?" ensure it only fires if someone is actually listening 
+        OnPlayerDeath?.Invoke();
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
