3388d91c7bc625cce7be64a9744df8a79c255bd3
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-04-10 13:29:31 +0530

    Logic for the coupled classes

--- MathsNewTut/Assets/Script/Practice script/CoupledChild.cs
+++ MathsNewTut/Assets/Script/Practice script/CoupledChild.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class CoupledChild : MonoBehaviour
 {
+    public void childManager()
+    {
+        //logic
+    }
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- MathsNewTut/Assets/Script/Practice script/CoupledParent.cs
+++ MathsNewTut/Assets/Script/Practice script/CoupledParent.cs
@@ -2,12 +2,26 @@ using UnityEngine;
 
 public class CoupledParent : MonoBehaviour
 {
+    public CoupledChild coupledChild;
+    public void takeDamage(float damage)
+    {
+        //logic 
+        die();
+    }
+
+    void die()
+    {
+        //logic 
+        
+        coupledChild.childManager();
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        coupledChild = GetComponent<CoupledChild>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
